a29a97f40a472e1a3bf58669c714fbcee8e0664e
roberttjia <robert@tjia.org>
2017-05-16 00:22:49 -0400

    Test

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prokore.Analysis.Rent;
+
+namespace Analyze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+        }
+
+        static void Test1()
+        {
+            var mr = new MarketRent();
+            mr.StartDt = new DateTime(2010, 01, 01);
+            mr.EndDt = new DateTime(2015, 12, 31);
+            mr.SquareFeet = 25000;
+            mr.InitialRate = 26;
+            mr.RentSculpt = new List<MarketRatePeriod>();
+            MarketRatePeriod mrp = new MarketRatePeriod();
+            mrp.StartDt = new DateTime(2010, 01, 01);
+            mrp.EndDt = new DateTime(2015, 12, 31);
+            mrp.GrowthRate = 7.0M; // 7% a year.
+            mr.RentSculpt.Add(mrp);
+            var op = mr.Analyze();
+            foreach (var pt in op)
+            {
+                Console.WriteLine(string.Format("{0} - {1}", pt.Date, pt.Amount));
+            }
+        }
+    }
+}
